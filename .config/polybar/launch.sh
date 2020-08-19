49bb3f44f69ae6742e3c6c87edb843b3e1607762
LordMero <graz@x1.localdomain>
2020-08-19 19:43:16 +0100

    some minor adj

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,15 +2,10 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar mainMonitor  >>/tmp/polybar1.log 2>&1 & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if [ "$(xrandr | awk '/HDMI1/ {print $2}')" == "connected" ]; then
-	polybar externalMonitor  >>/tmp/polybar1.log 2>&1 & disown
-fi
-
-echo "Bars launched..."
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainMonitor &
+done
